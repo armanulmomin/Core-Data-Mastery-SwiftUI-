da4322de1660abdb434bc09e4dc5528378ab148a
Armanul Momin <armanul.momin@gmail.com>
2025-08-06 09:43:09 +1000

    CoreData Swiftui

---
+++ CoreDataMastery/CoreDataMasteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataMasteryApp.swift
+//  CoreDataMastery
+//
+//  Created by Arman on 6/8/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataMasteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
